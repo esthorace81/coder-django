c6c442c041f00b33fb7da990ad2b26c30dd5268e
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2025-02-17 22:10:54 -0300

    feat: respuesta simple

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def saludar(request):
+    return HttpResponse("Hola desde Django!")
+
+
+def saludar_con_etiqueta(request):
+    return HttpResponse("<h1>Hola desde Django con etiquetas!</h1>")
