02d19c6370439f8554d8401ed46b246199bc7fb0
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2025-02-17 22:15:08 -0300

    feat: parámetros desde URL

--- src/core/views.py
+++ src/core/views.py
@@ -8,3 +8,9 @@ def saludar(request):
 
 def saludar_con_etiqueta(request):
     return HttpResponse("<h1>Hola desde Django con etiquetas!</h1>")
+
+
+def saludar_con_parametros(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize()  # Capitaliza la primera letra del nombre
+    apellido = apellido.capitalize()  # Capitaliza la primera letra del apellido
+    return HttpResponse(f"Hola {nombre} {apellido} desde Django con parámetros!")
