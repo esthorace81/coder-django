bc205a55e2ed3bf032d3780314ef5509a29616a3
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2025-03-10 21:57:36 -0300

    feat: personalizacion admin

--- src/cliente/admin.py
+++ src/cliente/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Pais)
-admin.site.register(models.Cliente)
+# admin.site.register(models.Cliente)
+
+
+@admin.register(models.Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "apellido", "nacimiento", "pais_origen")
+    search_fields = ("nombre", "apellido")
+    list_filter = ("pais_origen",)
+    ordering = ("apellido", "nombre")
+    date_hierarchy = "nacimiento"
