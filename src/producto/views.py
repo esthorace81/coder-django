4f539a863c53485410730b1bb6e9a8cdf028a4a2
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2025-03-17 21:46:40 -0300

    feat: CRUD categoria

--- src/producto/views.py
+++ src/producto/views.py
@@ -1,5 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from . import forms, models
 
 
 def index(request):
     return render(request, "producto/index.html")
+
+
+# CATEGORIA - LISTVIEW
+
+
+def categoria_list(request):
+    queryset = models.Categoria.objects.all()
+    context = {"object_list": queryset}
+    return render(request, "producto/categoria_list.html", context)
+
+
+# CATEGORIA - CREATEVIEW
+
+
+def categoria_create(request):
+    if request.method == "GET":
+        form = forms.CategoriaForm()
+    if request.method == "POST":
+        form = forms.CategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("producto:categoria_list")
+    return render(request, "producto/categoria_form.html", {"form": form})
+
+
+# CATEGORIA - UPDATEVIEW
+
+
+def categoria_update(request, pk: int):
+    query = models.Categoria.objects.get(id=pk)
+    if request.method == "GET":
+        form = forms.CategoriaForm(instance=query)
+    if request.method == "POST":
+        form = forms.CategoriaForm(request.POST, instance=query)
+        if form.is_valid():
+            form.save()
+            return redirect("producto:categoria_list")
+    return render(request, "producto/categoria_form.html", {"form": form})
+
+
+# CATEGORIA - DELETEVIEW
+
+
+def categoria_delete(request, pk: int):
+    query = models.Categoria.objects.get(id=pk)
+    if request.method == "POST":
+        query.delete()
+        return redirect("producto:categoria_list")
+    return render(request, "producto/categoria_confirm_delete.html", {"object": query})
